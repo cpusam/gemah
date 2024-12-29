c88531022633a2880586ccde1e97495706e2960d
Samuel Leonardo <samuel.leonardo19@gmail.com>
2024-12-29 19:01:08 -0300

    Atualização em 29/12/2024 às 19:01

--- testes/js/geradorExpressaoZ.js
+++ testes/js/geradorExpressaoZ.js
@@ -2,172 +2,227 @@
 ///////////////////////////////////////////////
 //gera exprões dentro dos números inteiros
 class GeradorExpressaoZ {
-	/**
+  /**
 	 * 
-	 * @param {{mult:[], div:[], sub:[], sum:[], qtdeOper: {}, digitsTermA: number}} optionsTable 
+	 * @param {{withParenteses:boolean, maxPotency:number, minNumber:number, maxNumber:number, maxLiteral:number, qtdeMonomio:number, oper:object}} optionsTable 
 	 */
 	constructor (optionsTable) {
 		this.optionsTable = optionsTable;
-		this.digitsTermA = optionsTable.digitsTermA;
-    this.digitsTermADiv = optionsTable.digitsTermADiv;
-		if (!this.optionsTable.digitsTermA)
-			alert("GeradorExpressaoZ::Quantidade de digitos termA é inválida");
-		this.answer = -1;
-		//tentativas para fazer a expressão
-		this.trying = 1000;
+    this.optionsTable.minNumber = 0;
+    this.optionsTable.maxNumber = 20;
+    this.optionsTable.qtdeMonomio = 5;
 		this.expressionStr = "";
-		this.qtdeLinhas = 0;
+		console.log(optionsTable.maxPotency, optionsTable.maxNumber, optionsTable.maxLiteral, optionsTable.qtdeMonomio);
 	}
 
 	reset (  ) {
-		this.answer = -1;
 		this.expressionStr = "";
-		this.qtdeLinhas = 0;
 	}
 
 	doExpression () {
 		this.reset();
-		let oper = [];
-		
-		if (this.optionsTable.mult.length)
-			oper.push('mult');
-		if (this.optionsTable.div.length)
-			oper.push('div');
-		if (this.optionsTable.sub.length)
-			oper.push('sub');
-		if (this.optionsTable.sum.length)
-			oper.push('sum');
-		
-		if (!oper.length === 0) {
-			alert("GeradorExpressaoZ.doExpression::oper é vazio");
-			return;
-		}
-
 		let expressionStr = "";
-		let qtdeOper = {}, qtdeAux = {};
 		
-		
-		let totalTerms = 0;
-		
-		let t = 0;
-    while (1) {
-      for (t = 0; t < this.trying; t++) {
-        expressionStr = "";
-        if (this.optionsTable.qtdeOper.mult)
-          qtdeOper["mult"] = RandInt(0, 2);
-        if (this.optionsTable.qtdeOper.div)
-          qtdeOper["div"] = RandInt(0, 2);
-        if (this.optionsTable.qtdeOper.sub)
-          qtdeOper["sub"] = RandInt(1, 3);
-        if (this.optionsTable.qtdeOper.sum)
-          qtdeOper["sum"] = RandInt(1, 3);
+		//em 01/02/2023 às 15:56
+    //a = um número qualquer escolhido
+    //b = um número qualquer escolhido que vem depois de um 'a' ou 'A' escolhido
+		//s = sinal + ou -
+    //n = sinal negativo
+		//A = quadrado de um número a
+    //B = quadrado de um número b
+    //D = 2 vezes 'a' vezes 'b'
+		//p = potência de 2
+		//r = raiz quadrada de q
+    //i = iniciar tudo para a expressão seguinte, tipo resetar valores para usar nos próximos tokenbs do formato
+    //Q = dividendo tabuada de vididir
+    //q = divisor de 'q' tabuada de dividir 
+    //NOTA: entre cada formato é escolhido um sinal s
+    //NOTA: a potencia tem que ficar por último fora da expressão pro js executar corretamente (defeito)
+    //NOTA: 'a' é maior que 'b'
+		const formatosBase = this.optionsTable.formatosBase || [
+      'a +b',
+      'a -b',
+      'a sb',
+		];
+    if (this.optionsTable.oper["mult"]) {
+      formatosBase.push('a * (sb)');
+      formatosBase.push('a * (-b)');
+    }
+    if (this.optionsTable.oper["div"]) {
+      formatosBase.push('Q / (sq)');
+      formatosBase.push('Q / (-q)');
+    }
 
-        totalTerms = 0;
-        for (let o in qtdeOper) {
-          totalTerms += qtdeOper[o];
-        }
+    const formatoSemParenteses = this.optionsTable.formatosSemParenteses || [
+      'a + b',
+		];
+
+		let formatos = formatosBase;//this.optionsTable.withParenteses? formatosBase: formatoSemParenteses;
+
+    const sinal = ['-', '+'];
+    const sinalLink = ['+', '-'];
+		
+		
+    console.log(this.optionsTable.qtdeMonomio);
+		for (let t = 0; t < 100; t++) {
+			let expUser = '';//expressão para o usuário
+      let expJS = '';//expressão para o javascript calcular
+			
+			let chosenFormas = [];
+      const maxMonomio = this.optionsTable.qtdeMonomio;// RandInt(2, this.optionsTable.qtdeMonomio);
+			for (let total = 0, lastMonomio = maxMonomio - 1; total < maxMonomio; total++) {
+        //escolhe a forma
+				let forma = formatos[RandInt(0, formatos.length - 1)];
+        chosenFormas.push(forma);
+        let last = '';
+        let opLink = sinalLink[RandInt(0, sinalLink.length - 1)];
         
-        if (!totalTerms) {
-          continue;
-        }
+        console.log('formato escolhido = '+forma);
+        let a = 0;
+        let A = 0;
+        let b = 0;
+        let B = 0;
+        let s = '';
 
-        for (let o in qtdeOper) {
-          qtdeAux[o] = qtdeOper[o];
-        }
-        for (let i = 0, total = totalTerms; ; ) {
-          total = 0;
-          for (let o in qtdeAux)
-            total += qtdeAux[o];
-          if (total === 0) {
-            t = this.trying;
-            break;
-          }
+        let hasPotency = forma.indexOf('^') > -1;
+        if (hasPotency)
+          expJS += 'Math.pow(';
           
-          let chosen = RandInt(0, oper.length - 1);
-          let sinal = "";
-          let options = [];
-          switch (oper[chosen]) {
-            case 'mult':
-              {
-                if (qtdeAux["mult"] > 0) {
-                  qtdeAux["mult"]--;
-                  this.qtdeLinhas++;
-                  sinal = "*";
-                  options = this.optionsTable.mult;
-                }
-              }
-              break;
-            case 'div':
-              {
-                if (qtdeAux["div"] > 0) {
-                  qtdeAux["div"]--;
-                  this.qtdeLinhas++;
-                  sinal = "/";
-                  options = this.optionsTable.div;
-                }
-              }
-              break;
-            case 'sub':
-              {
-                if (qtdeAux["sub"] > 0) {
-                  this.qtdeLinhas += 2;
-                  sinal = "-";
-                  options = this.optionsTable.sub;
-                  qtdeAux["sub"]--;
-                }
-              }
-              break;
-            case 'sum':
-              {
-                if (qtdeAux["sum"] > 0) {
-                  this.qtdeLinhas += 2;
-                  sinal = "+";
-                  options = this.optionsTable.sum;
-                  qtdeAux["sum"]--;
-                }
-              }
-              break;
+
+				{
+          let tokens = [];
+          a = RandInt(this.optionsTable.minNumber, this.optionsTable.maxNumber);
+          A = a * a;
+          let ok = false;
+          while (!ok) {
+            b = RandInt(this.optionsTable.minNumber, this.optionsTable.maxNumber);
+            B = b * b;
+            ok = b <= a;
           }
 
-          if (!options.length) {
-            continue;
-          }
+          s = sinal[RandInt(0, sinal.length - 1)];
+          
+          //formato das expressões binárias
+          //t = termo de variável aleatória
+          //l = valor lógico true ou false (V ou F para o usuário)
+          //o = operador binário pode ser && ou || (E ou OU para o user)
+          //i = inverso lógico (negação)
+          let Q = 0, q = 0;
+            for (let j = 0; forma.charAt(j); j++) {
+              switch (forma.charAt(j)) {
+                case 'a':
+                  a = RandInt(this.optionsTable.minNumber, this.optionsTable.maxNumber);
+                  A = a * a;
+                  expUser += ' '+a;
+                  expJS += ' '+a;
+                  tokens.push(a);
+                  break;
+                case 'b':
+                  ok = false;
+                  while (!ok) {
+                    b = RandInt(this.optionsTable.minNumber, this.optionsTable.maxNumber);
+                    B = b * b;
+                    ok = b <= a;
+                  }
+                  expUser += ' '+b;
+                  expJS += ' '+b;
+                  tokens.push(b);
+                  break;
+                case 'A':
+                  expUser += ' '+A;
+                  expJS += ' '+A;
+                  tokens.push(A);
+                  break;
+                case 'B':
+                  expUser += ' '+B;
+                  expJS += ' '+B;
+                  tokens.push(B);
+                  break;
+                case'D':
+                  {
+                    let D = 2 * a * b;
+                    expUser += ' '+D;
+                    expJS += ' '+D;
+                    tokens.push(D);
+                  }
+                  break;
+                case 's':
+                  {
+                    expUser += ' '+s;
+                    expJS += ' '+s;
+                    last = 's';
+                    tokens.push(s);
+                  }
+                  break;
+                case 'n': 
+                  {
+                    expUser += ' '+'-';
+                    expJS += '-';
+                    tokens.push('n');
+                  }
+                  break;
+                //não coloca esses chars
+                case '^':
+                case 'p':
+                  expUser += '^2';
+                  tokens.push('^2');
+                  break;
+                case 'Q':{
+                    let a = RandInt(1,9);
+                    let b = RandInt(1,9);
+                    Q = a * b;
+                    if (RandInt(0,1))
+                      q = a;
+                    else
+                      q = b;
+                    B = Q;
+                    expUser += ' '+B;
+                    expJS += ' '+B;
+                    tokens.push(B);
+                  }
+                  break;
+                case 'q':{
+                    B = q;
+                    expUser += ' '+B;
+                    expJS += ' '+B;
+                    tokens.push(B);
+                  }
+                  break;
+                default:
+                  expUser += ' '+forma.charAt(j);
+                  expJS += ''+forma.charAt(j);
+                  tokens.push(forma.charAt(j));
+                  break;
+              }
+            }
 
-          let termA = parseInt(''+makeMultTerm(0,9, this.digitsTermA, false));
-          if (oper[chosen] === 'div') {
-            parseInt(''+makeMultTerm(0,9, this.digitsTermADiv, false));
+          if (last !== 'e') {
+            console.log('['+total+'] = '+last+' exp \''+expUser+'\'');
+            console.log('['+total+'] = '+last+' exp \''+expJS+'\'');
           }
-          let termB = parseInt(options[RandInt(0, options.length - 1)]);
-          if (termA == 'NaN' || termB === 'NaN'){
-            alert("GeradorExpressaoZ.doExpression::Erro, termos inválidos, termA="+termA+', termB='+termB);
-            return false;
-          }
-          /**
-          if (oper[chosen] === "sub" && termB > termA) {
-            let aux = termB;
-            termB = termA;
-            termA = aux;
-          }
-          */
+				}
 
-          if (oper[chosen] === "div" && Math.fmod(termA, termB) > 0) {
-            termA = RandInt(0, 9) * termB;
-          }
-
-          expressionStr += ' '+termA+' '+sinal+' '+termB;
-          if (total > 1) {
-            expressionStr += RandInt(0, 1)?' + ': ' - ';
-            this.qtdeLinhas++;
-          }
+        if (hasPotency) {
+          expJS += ','+'2'+')';
         }
-        console.log(expressionStr);
-        this.answer = eval(expressionStr);
-        if (this.answer > 0) {
-          this.expressionStr = expressionStr;
-          return true;
+
+        if (total != lastMonomio) {
+          expUser += ' '+opLink;
+          expJS += ''+opLink; 
         }
       }
-    }
+
+			console.log('Gerador expressão Numérica notável = \'' + expJS + "'");
+      const resp = eval(expJS);
+			if (resp !== 'undefined') {
+
+        this.expressionStr = expUser;
+        this.expressionStrJS = expJS;
+        this.answer = resp;
+				//this.answer = this.generateAnswer({chosenFormas, answerMonomioBase})
+				return true;
+			}
+		}
 
 		return false;
 	}
@@ -189,6 +244,7 @@ function gerarExpressaoZ ( form, targetId ) {
     sub.push(i);
     div.push(i);
   }
+  let oper = {};
 
 	let digitsTermA = 1;
   let digitsTermADiv = 2;
@@ -200,15 +256,19 @@ function gerarExpressaoZ ( form, targetId ) {
       terms = 1;
       if (el.id.indexOf('chosenMult') > -1) {
         qtdeOper["mult"] = RandInt(0, 2);
+        oper['mult'] = 1;
       }
       else if (el.id.indexOf('chosenDiv') > -1) {
         qtdeOper["div"] = RandInt(0, 2);
+        oper['div'] = 1;
       }
       else if (el.id.indexOf('chosenSub') > -1) {
         qtdeOper["sub"] = RandInt(1, 3);
+        oper['sub'] = 1;
       }
       else if (el.id.indexOf('chosenSum') > -1) {
         qtdeOper["sum"] = RandInt(1, 3);
+        oper['sum'] = 1;
       }
     }
     else if (el.id.indexOf('qtdeExpressao') > -1) {
@@ -226,14 +286,14 @@ function gerarExpressaoZ ( form, targetId ) {
     return;
   }
 
-	let gerador = new GeradorExpressaoZ({mult,div, sub, sum, digitsTermA, digitsTermADiv, qtdeOper});
+	let gerador = new GeradorExpressaoZ({oper, digitsTermA, digitsTermADiv, qtdeOper});
 	let strHtml = "";
 	for (let i = 0; i < qtdeExpressao; i++) {
 		if (!gerador.doExpression()) {
 			console.log('Falha ao gerar expressão N i='+i, gerador);
 			continue;
 		}
-		strHtml += '<p><strong>NOTA: Para a expressão, deixe '+(gerador.qtdeLinhas - 1)+' linhas no caderno abaixo da expressão</strong><br />';
+		//strHtml += '<p><strong>NOTA: Para a expressão, deixe '+(gerador.qtdeLinhas - 1)+' linhas no caderno abaixo da expressão</strong><br />';
 		strHtml += 'Expressão N-'+(i+1)+': '
 		+'<strong>'
 		+gerador.expressionStr
